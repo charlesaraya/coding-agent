ea3da80aafd42344fdbe6b8cf936dd038f0a6f7e
charlesaraya <charlesaraya@gmail.com>
2025-06-22 11:38:34 +0200

    refactor: improve error handling

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -3,26 +3,24 @@ import os
 MAX_READ_STREAM = 10_000
 
 def get_file_content(working_directory, file_path):
-    wd_path = os.path.abspath(working_directory)
-    path = os.path.join(wd_path, file_path)
-
-    # ensure we stay in the current dir
-    if not path.startswith(wd_path) or file_path.startswith("../"):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-
-    if not os.path.exists(path) or not os.path.isfile(path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-
+    if os.path.exists(abs_file_path):
+        return f'Error: {abs_file_path} does not exist'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: {abs_file_path} is not a file'
     try:
-        with open(path, "r", encoding="utf-8") as f:
-            read_data = f.read(MAX_READ_STREAM)
-            if len(read_data) == MAX_READ_STREAM:
-                read_data += f'[...File "{file_path}" truncated at 10000 characters]'
+        with open(abs_file_path, "r", encoding="utf-8") as f:
+            content = f.read(MAX_READ_STREAM)
+            if len(content) == MAX_READ_STREAM:
+                content += f'[...File "{file_path}" truncated at 10000 characters]'
+        return content
     except Exception as e:
         return f'Error: {e}'
 
-    return read_data
 
 if __name__ == '__main__':
-    read_data = get_file_content('calculator', '/bin/cat')
-    print(read_data)
+    out = get_file_content('calculator', '/bin/cat')
+    print(out)
